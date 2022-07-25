938c2986a75f7f40f2dd5e9f88e4d3e9c99328f6
Ryan Melo <68710321+ryanvmelo@users.noreply.github.com>
2022-07-25 10:58:03 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+const cacheName = 'portfolio'
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            cache.addAll([
+                './',
+                './index.html',
+                './manifest.webmanifest',
+                './index.js',
+                './cadastrar.html',
+                './embed.html',
+                './login.html',
+                './musicas.html',
+                './perfis.html',
+                './senha.html',
+                './music-player.html',
+                './music-player2.html',
+                './music-player3.html'
+            ])
+        })
+    )
+    return self.skipWaiting()
+})
+
+self.addEventListener('activate', e =>{
+    self.clients.claim()
+})
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else{
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached || fetch(req)
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req);
+        return cached
+    }
+}
